f3217a4f0dfc4f8112963cd3aced1429d9a67ff7
Simplyareed <adam.reed1991@gmail.com>
2024-07-31 20:37:32 -0700

    adding files for personal react portfolio, unfinished

---
+++ server.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// // Define the port to listen on
+// const PORT = process.env.PORT || 3000;
+
+// // Serve static files from the 'build' directory
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// // Catch-all route to serve the index.html for any requests
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
